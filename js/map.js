c1b4058f49edba36d57f597408e25dc94e019a49
Nedžad Beus <67960691+vekejsn@users.noreply.github.com>
2023-03-31 00:44:03 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -26,7 +26,7 @@ window.onload = async () => {
     }
     map = new maplibregl.Map({
         container: 'map',
-        style: 'https://api.maptiler.com/maps/streets/style.json?key=' + keys,
+        style: 'https://tiles.derp.si/maps/streets-v2/style.json',
         center: [18,44.4110628],
         zoom: 7,
         minZoom: 5,
