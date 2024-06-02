9bb4d80f7d492505d92a1d863a8640bd05715585
Miha Frangež <miha.frangez@gmail.com>
2024-06-02 21:41:06 +0200

    3D buildings are already in the style

--- js/map.js
+++ js/map.js
@@ -58,59 +58,6 @@ window.onload = async () => {
     map.addControl(new maplibregl.NavigationControl());
 
     map.on('load', function () {
-        //
-        // 3D buildings
-        //
-
-        // Insert the layer beneath any symbol layer.
-        var layers = map.getStyle().layers;
-
-        var labelLayerId;
-        for (var i = 0; i < layers.length; i++) {
-            if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-                labelLayerId = layers[i].id;
-                break;
-            }
-        }
-
-
-        map.addLayer(
-            {
-                'id': '3d-buildings',
-                'source': 'openmaptiles',
-                'source-layer': 'building',
-                'filter': ['==', 'extrude', 'true'],
-                'type': 'fill-extrusion',
-                'minzoom': 15,
-                'paint': {
-                    'fill-extrusion-color': '#aaa',
-    
-                    // use an 'interpolate' expression to add a smooth transition effect to the
-                    // buildings as the user zooms in
-                    'fill-extrusion-height': [
-                        'interpolate',
-                        ['linear'],
-                        ['zoom'],
-                        15,
-                        0,
-                        15.05,
-                        ['get', 'height']
-                    ],
-                    'fill-extrusion-base': [
-                        'interpolate',
-                        ['linear'],
-                        ['zoom'],
-                        15,
-                        0,
-                        15.05,
-                        ['get', 'min_height']
-                    ],
-                    'fill-extrusion-opacity': 0.6
-                }
-            },
-            labelLayerId
-        );
-
         // custom rail styling:
         for (var i = 0; i < layers.length; i++) {
             if (layers[i].type === 'line' && layers[i].id.endsWith('major_rail')) {
